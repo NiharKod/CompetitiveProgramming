3c64677d0831dbbef9a71f8c779bfd9a219fc502
Nihar Kodkani <38077916+NiharKod@users.noreply.github.com>
2020-05-26 14:59:04 -0400

    Add files via upload

---
+++ watermelon.java
@@ -0,0 +1,34 @@
+//package com.company;
+
+
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class watermelon {
+    public static void main (String [] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+
+        if(w % 2 == 0){
+            if(w<=2){
+                System.out.println("NO");
+            }
+            else {
+                System.out.println("YES");
+            }
+        }else{
+            System.out.println("NO");
+        }
+
+
+        //yeet
+
+
+
+
+
+    }
+
+}
